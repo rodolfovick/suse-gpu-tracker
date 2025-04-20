bd48672b4abe0c19ba8d58ea8ca2ca8e3aab97fc
Rodolfo Vick <rodolfo.vick@gmail.com>
2025-04-20 20:55:59 -0300

    Adding new gpu-nodes to list when a new node is created

--- docker/gputracker-operator.py
+++ docker/gputracker-operator.py
@@ -14,17 +14,28 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     is created, create a new one. Otherwise, just add the new gpu-node to the list.
     """
 
-    path = os.path.join(os.path.dirname(__file__), GPU_TRACKER_OBJ)
-    tmpl = open(path, 'rt').read()
-    text = tmpl.format(name=name)
-    data = yaml.safe_load(text)
+    logger.info(f'::: NAME {name}')
 
     dynamicClient = kubernetes.dynamic.DynamicClient(kubernetes.client.ApiClient())
     resource = dynamicClient.resources.get(api_version='suse.tests.dev/v1', kind='GPUTracker')
-    object = resource.create(body=data, namespace=namespace)
+    objectList = resource.get(namespace=namespace)
 
-    logger.info(f"GPU Tracker created: {object}")
-
+    if not objectList.items:
+        path = os.path.join(os.path.dirname(__file__), GPU_TRACKER_OBJ)
+        tmpl = open(path, 'rt').read()
+        text = tmpl.format(name=name)
+        data = yaml.safe_load(text)
+        object = resource.create(body=data, namespace=namespace)
+        logger.info(f"GPU Tracker created: {object}")
+    else:
+        object = resource.get(name='suse-gpu-tracker', namespace=namespace)
+        gpuNodesList = object.gpu_nodes.split(',')
+        gpuNodesList.append(name)
+        gpuNodes = ', '.join(gpuNodesList)
+        object = resource.patch(body={'gpu_nodes': gpuNodes}, name='suse-gpu-tracker', 
+                       namespace=namespace, content_type='application/merge-patch+json')
+        logger.info(f"GPU Tracker updated: {object}")
+        
 
 @kopf.on.delete('Node', labels={'node-type': 'gpu-node'})
 def delete_fn(spec, name, namespace, logger, **kwargs):
@@ -35,6 +46,7 @@ def delete_fn(spec, name, namespace, logger, **kwargs):
 
     dynamicClient = kubernetes.dynamic.DynamicClient(kubernetes.client.ApiClient())
     resource = dynamicClient.resources.get(api_version='suse.tests.dev/v1', kind='GPUTracker')
+    
     object = resource.get(name='suse-gpu-tracker', namespace=namespace)
     resource.delete(name='suse-gpu-tracker', namespace=namespace)
 
