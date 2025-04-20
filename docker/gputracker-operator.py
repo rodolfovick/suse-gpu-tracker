f26d6af5ff5c4a8bb2131fa932db07255cdf898d
Rodolfo Vick <rodolfo.vick@gmail.com>
2025-04-19 21:59:17 -0300

    Adding logic to create/delete gpu-nodes

--- docker/gputracker-operator.py
+++ docker/gputracker-operator.py
@@ -1,6 +1,41 @@
+import os
 import kopf
 import logging
+import kubernetes
+import yaml
 
-@kopf.on.create('GPUTracker')
-def create_fn(body, **kwargs):
-    logging.info(f"A handler is called with body: {body}")
+GPU_TRACKER_OBJ = 'gputracker-obj.yml'
+
+
+@kopf.on.create('Node', labels={'node-type': 'gpu-node'})
+def create_fn(spec, name, namespace, logger, **kwargs):
+    """
+    Handle new Nodes with node-type equals to gpu-node. If no suse-gpu-tracker obj 
+    is created, create a new one. Otherwise, just add the new gpu-node to the list.
+    """
+
+    path = os.path.join(os.path.dirname(__file__), GPU_TRACKER_OBJ)
+    tmpl = open(path, 'rt').read()
+    text = tmpl.format(name=name)
+    data = yaml.safe_load(text)
+
+    dynamicClient = kubernetes.dynamic.DynamicClient(kubernetes.client.ApiClient())
+    resource = dynamicClient.resources.get(api_version='suse.tests.dev/v1', kind='GPUTracker')
+    object = resource.create(body=data, namespace=namespace)
+
+    logger.info(f"GPU Tracker created: {object}")
+
+
+@kopf.on.delete('Node', labels={'node-type': 'gpu-node'})
+def delete_fn(spec, name, namespace, logger, **kwargs):
+    """
+    Handle the delete of Nodes with node-type equals to gpu-node. If there are more gpu-node to 
+    suse-gpu-tracker, just remove from list. Otherwise, delete suse-gpu-tracker.
+    """
+
+    dynamicClient = kubernetes.dynamic.DynamicClient(kubernetes.client.ApiClient())
+    resource = dynamicClient.resources.get(api_version='suse.tests.dev/v1', kind='GPUTracker')
+    object = resource.get(name='suse-gpu-tracker', namespace=namespace)
+    resource.delete(name='suse-gpu-tracker', namespace=namespace)
+
+    logger.info(f"GPU Tracker deleted: {object}")
